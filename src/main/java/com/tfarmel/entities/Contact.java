e2435866542a7e660ab8982fdc59e39f612246ad
tfarmel <tfarmel@yahoo.fr>
2017-10-29 03:48:12 +0100

    modif class contact

--- src/main/java/com/tfarmel/entities/Contact.java
+++ src/main/java/com/tfarmel/entities/Contact.java
@@ -1,7 +1,17 @@
 package com.tfarmel.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 
-public class Contact extends User implements Serializable {
+public class Contact implements Serializable {
+	
+	private Long id;
+	private String nom;
+	private String prenom;
+	private Date dateNaissance;
+	private String email;
+	private long tel;
+	
+	
 
 }
